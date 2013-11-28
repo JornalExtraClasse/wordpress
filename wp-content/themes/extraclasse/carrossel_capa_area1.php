a5e8739b7fb6e417fbd2e80fc4d9a6f4e785ca4d
cwidiego <diego@sinprors.org.br>
2013-11-28 17:22:07 -0200

    Alteração Tema Extraclasse
    Cria página carrosel
    Edição da Home

---
+++ wp-content/themes/extraclasse/carrossel_capa_area1.php
@@ -0,0 +1,5 @@
+<div class="">&nbsp;</div>
+<h1>Carrossel de Destaques</h1>
+<img src="<?php the_field('imagem_1', '175'); ?>" alt="" />
+<p><?php the_field('manchete_1', '175'); ?></p>
+<p><?php the_field('linha_de_apoio_1', '175'); ?></p>
---
+++ wp-content/themes/extraclasse/home.php
@@ -0,0 +1,7 @@
+﻿
+<?php
+// CARREGA CARROUSSEL DE DESTAQUES
+require_once('carrossel_capa_area1.php'); 
+?>
+
+<?php get_footer(); ?>
